03eedf213a1c00f1270f3b001b57a4fb0a7e2c38
Matthew Hebert <soyrux@gmail.com>
2015-12-01 02:12:08 -0500

    Make structures placeable using cursor, add R_Pentomino

--- src/main/java/com/daexsys/automata/PlayerState.java
+++ src/main/java/com/daexsys/automata/PlayerState.java
@@ -1,10 +1,13 @@
 package com.daexsys.automata;
 
+import com.daexsys.automata.world.structures.Structure;
+import com.daexsys.automata.world.structures.Structures;
 import com.daexsys.automata.world.tiletypes.TileType;
 import com.daexsys.automata.world.tiletypes.TileTypes;
 
 public class PlayerState {
 
+    private Structure selectedStructure = Structures.CGOL_R_PENTOMINO;
     private TileType inHand = TileTypes.CGOL;
 
     public TileType getInHand() {
@@ -13,5 +16,9 @@ public class PlayerState {
 
     public void setInHand(TileType inHand) {
         this.inHand = inHand;
+    }
+
+    public Structure getSelectedStructure() {
+        return selectedStructure;
     }
 }
