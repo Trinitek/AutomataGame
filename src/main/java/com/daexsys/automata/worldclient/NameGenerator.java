d018210d60b75bddcb563d4d03cecf14dce35fdb
Matthew Hebert <soyrux@gmail.com>
2015-12-04 01:20:41 -0500

    More names!!

--- src/main/java/com/daexsys/automata/worldclient/NameGenerator.java
+++ src/main/java/com/daexsys/automata/worldclient/NameGenerator.java
@@ -18,6 +18,19 @@ public class NameGenerator {
         names.add("Aquatic");
         names.add("Surreal");
         names.add("Cognizant");
+        names.add("Capricious");
+        names.add("Ambiguous");
+        names.add("Ephemeral");
+        names.add("Reflective");
+        names.add("Snowy");
+        names.add("Prodigal");
+        names.add("Lamenting");
+        names.add("Midnight");
+        names.add("Bitwise");
+        names.add("Patient");
+        names.add("Euphoric");
+        names.add("Shrouded");
+        names.add("Astral");
 
         List<String> names2 = new ArrayList<>();
         names2.add("Panda");
@@ -28,6 +41,17 @@ public class NameGenerator {
         names2.add("Wave");
         names2.add("Ripple");
         names2.add("Mothman");
+        names2.add("Paladin");
+        names2.add("Arachnid");
+        names2.add("Shimmer");
+        names2.add("Lemon");
+        names2.add("Wanderlust");
+        names2.add("Meme");
+        names2.add("Fish");
+        names2.add("Witch");
+        names2.add("Cryptid");
+        names2.add("Oracle");
+        names2.add("Automaton");
 
         return names.get(random.nextInt(names.size())) + names2.get(random.nextInt((names2.size())));
     }
